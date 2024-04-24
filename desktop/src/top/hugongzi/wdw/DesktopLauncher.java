fd5b9df8d8939272e597ad5587c51f1f6a54431a
狐白 <dream.qu@qq.com>
2024-04-24 19:44:23 +0800

    version 0.1 upload files

---
+++ desktop/src/top/hugongzi/wdw/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package top.hugongzi.wdw;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import top.hugongzi.wdw.GameEntry;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("WDWorld_Libgdx");
+		new Lwjgl3Application(new GameEntry(), config);
+	}
+}
