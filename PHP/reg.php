0eab28fa5d20639b26f84a269a4173fbf80263dd
狐白 <dream.qu@qq.com>
2024-04-29 10:59:32 +0800

    Version t0.2.8(test0.2.8) 新增注册页和登录页的注册逻辑 修改了post方法，现在仅由服务器返回值确定状态 支持登录和注册了 gradlew更新权限

--- PHP/reg.php
+++ PHP/reg.php
@@ -8,23 +8,23 @@ if (!isset($_GET['username']) ||
     !isset($_GET['email']) ||
     !isset($_GET['version'])
 ) {
-    die('参数错误');
+    die('fail|参数错误');
 }
 $username = $_GET['username'];
 $password = $_GET['password'];
 $name = $_GET['name'];
 $mail = $_GET['email'];
 $version = $_GET['version'];
-if (empty($username)) die('账号不能为空');
-if (strlen($username) > 18 || strlen($username) < 4) die('账号长度为4-18个字节');
-if (!preg_match("/^[a-z\s]+$/", $username[0])) die('账号必须以小写字母开头');
-if (empty($name)) die('用户名不能为空');
-if (empty($password)) die('密码不能为空');
-if (empty($mail)) die('邮箱不能为空');
-if (!preg_match("/^[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[-_a-z0-9]+\.)*[a-z0-9][-a-z0-9]{0,62}\.(?:[a-z]{2}\.)?[a-z]{2,}$/i", $mail)) {
-    die('邮箱地址格式错误');
+if (empty($username)) die('fail|账号不能为空');
+if (strlen($username) > 18 || strlen($username) < 4) die('fail|账号长度为4-18个字节');
+if (!preg_match("/^[a-z\s]+$/", $username[0])) die('fail|账号必须以小写字母开头');
+if (empty($name)) die('fail|用户名不能为空');
+if (empty($password)) die('fail|密码不能为空');
+if (empty($mail)) die('fail|邮箱不能为空');
+if (!preg_match('/\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/', $mail)) {
+    //die("邮箱不合法");
 }
-if ($version != $ver) die('客户端验证失败:版本不兼容');
+if ($version != $ver) die('fail|客户端验证失败:版本不兼容');
 $database = GetDatabase();
 $res = $database->get('users', ['username',], ['username[=]' => $username]);
 if (!$res) {
@@ -34,8 +34,8 @@ if (!$res) {
         'name' => $name,
         'email' => $mail
     ]);
-    if ($res) echo '注册成功';
-    else echo '注册失败';
+    if ($res) echo 'success|注册成功';
+    else echo 'fail|注册失败';
 } else {
-    echo '账号已经被注册';
+    echo 'fail|账号已经被注册';
 }
